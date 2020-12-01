17c24d3dfef03a65e84e8f6f4fc2edfdfa448e0f
ArcherV1nce <glowbradyon@gmail.com>
2020-12-01 13:38:24 +0200

    WIP

---
+++ Scripts/System/Grid.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Grid {
+
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
+
+    private TextMesh[,] debugArrayText;
+
+    public Grid (int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        debugArrayText = new TextMesh[width, height];
+        #region Shows Grid
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                Debug.Log("x = " + x + " y = " + y);
+
+                debugArrayText[x,y] = CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x,y) + new Vector3 (cellSize, cellSize) * 0.5f, 30, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.grey, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.grey, 100f);
+            }
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.grey, 100f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.grey, 100f);
+        
+        }
+
+        SetValue(1, 2, 23);
+
+        Debug.Log("With: " + width + " Height: " + height);
+        #endregion
+    }
+
+    public void SetValue (int x, int y, int value)
+    {
+        if(x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugArrayText[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    #region Create text in the game world
+
+    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 10, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = 5000)
+    {
+        if (color == null) color = Color.white;
+        return CreateWorldText (parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
+    }
+
+    public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+
+    }
+
+    #endregion
+
+    Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+}
---
+++ Scripts/System/GridTesting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Grid grid = new Grid(20, 10, 5f);
+
+
+    }
+
+}
