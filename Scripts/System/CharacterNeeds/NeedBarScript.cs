3871ea7c22be8965fd432575afc18af4b1ffd126
olena <olena.krysan@gmail.com>
2020-12-06 06:22:18 +0200

    Add player's basic needs

---
+++ Scripts/System/CharacterNeeds/NeedBarScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NeedBarScript : MonoBehaviour
+{
+
+    [Header("NeedBar")]
+    [Range(1f, 15f)] [SerializeField] private float needBarStartMaxValue = 10f;
+    [Range(0.05f, 1f)] [SerializeField] private float needBarStartLossingAmount = 0.1f;
+    [Range(1f, 20f)] [SerializeField] private float needBarPeriodBerweenTrigger = 2f;
+
+    public NeedBar needsBar;
+    public Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        needsBar = new NeedBar(needBarStartLossingAmount, needBarPeriodBerweenTrigger, needBarStartMaxValue);
+        slider.maxValue = needsBar.maxValue;
+        var coroutine = needsBar.GetLosingPointsEnumerator();
+        StartCoroutine(coroutine);
+    }
+
+    void Update()
+    {
+        slider.value = needsBar.currentValue;
+    }
+
+}
---
+++ Scripts/System/CharacterNeeds/NeedsBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedBar
+{
+    public float oneActionLosingAmount;
+    public float periodBetweenTrigger;
+    public float maxValue;
+    public float currentValue;
+
+    public NeedBar(float oneActionLosingAmount, float periodBetweenTrigger, float maxValue)
+    {
+        this.oneActionLosingAmount = oneActionLosingAmount;
+        this.periodBetweenTrigger = periodBetweenTrigger;
+        this.maxValue = maxValue;
+        this.currentValue = maxValue;
+    }
+
+    public void FillBar(float amount)
+    {
+        if (currentValue + amount > maxValue)
+        {
+            currentValue = maxValue;
+        }
+        else
+        {
+            currentValue += amount;
+        }
+    }
+
+    public void SetMaxValue(float amount)
+    {
+        maxValue = amount;
+    }
+
+    public IEnumerator GetLosingPointsEnumerator()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(periodBetweenTrigger);
+            currentValue -= oneActionLosingAmount;
+            if (currentValue < 0)
+            {
+                currentValue = 0;
+            }
+        }
+    }
+
+}
